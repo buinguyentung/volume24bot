4131b604a34eee6d03809516687d618e2bcdead3
tandinho <hodinhtan@gmail.com>
2018-10-25 23:29:39 +0700

    change testupdate

--- cmarket/cmarket.py
+++ cmarket/cmarket.py
@@ -30,21 +30,21 @@ client = datastore.Client()
 key = client.key('TenX')
 item = datastore.Entity(key)
 item.update({
-  		'market_cap_usd': '63720958.0',
-  		'price_usd': '0.5827362112',
-  		'last_updated': '1540476750',
+  		'market_cap_usd': 63720958.0,
+  		'price_usd': 0.5827362112,
+  		'last_updated': 1540476750,
   		'name': 'TenX',
-  		'24h_volume_usd': '788879.398331',
-  		'percent_change_7d': '2.24',
+  		'24h_volume_usd': 788879.398331,
+  		'percent_change_7d': 2.24,
   		'symbol': 'PAY',
   		'max_supply': 'None',
-  		'rank': '100',
-  		'percent_change_1h': '-0.1',
-  		'total_supply': '205218256.0',
-  		'price_btc': '0.00009027',
-  		'available_supply': '109347861.0',
-  		'percent_change_24h': '1.48',
-  		'id': 'tenx'
+  		'rank': 100,
+  		'percent_change_1h': -0.1,
+  		'total_supply': 205218256.0,
+  		'price_btc': 0.00009027,
+  		'available_supply': 109347861.0,
+  		'percent_change_24h': 1.48,
+  		 'id': 'tenx'
 })
 key = client.put(item)
 # {

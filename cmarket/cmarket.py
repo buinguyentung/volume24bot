b5a4c1b04ef7ad6c660a08bb60106428b2e1bcf3
tan1.ho <tan1.ho@lge.com>
2018-10-25 17:21:42 +0700

    add to struct

--- cmarket/cmarket.py
+++ cmarket/cmarket.py
@@ -2,8 +2,17 @@ import json
 import requests
 
 r = requests.get('https://api.coinmarketcap.com/v1/ticker/')
+class cstruct:
+    def __init__(self, id, symbol, rank):
+        self.id = id
+        self.symbol = symbol
+        self.rank = rank
+carray = []
 for coin in r.json():
-    print(coin["id"])
-    print("  "+coin["rank"])
-    print("  "+coin["symbol"])
-    
+    p = cstruct(coin["id"], coin["symbol"], coin["rank"]);
+    carray.append(p)
+
+for x in carray:
+    print(x.id)
+    print(x.symbol)
+    print(x.rank)

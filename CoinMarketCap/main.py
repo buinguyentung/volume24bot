f867c6544d44f194c82ff36e7c77bf4751f0171f
admin-compute <hodinhtan@gmail.com>
2018-11-11 16:24:17 +0000

    Code:Task5: update shedule time

--- CoinMarketCap/main.py
+++ CoinMarketCap/main.py
@@ -10,7 +10,8 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/hodinhtan/volume24bot/CoinMa
 #/home/tanho/github/py/volume24bot/cmarket
 client = firestore.Client()
 
-lt = {'bitcoin','ethereum','ripple','litecoin','dogecoin'}
+lt = {'bitcoin','ethereum','ripple','litecoin','dogecoin',
+        "bitcoin-cash","eos","stellar","cardano","monero","tether"}
   
 def urlCoin(name):
   return requests.get('https://api.coinmarketcap.com/v1/ticker/'+name)
@@ -21,19 +22,23 @@ def sampling_data():
     for name in lt:
         req = urlCoin(name)
         data = req.json()
-        date_time = unicode(datetime.datetime.now().date())
-        updatedtime = unicode(datetime.datetime.now())
-        data[0]["datetime"] = date_time
-        data[0]["updatedtime"] = updatedtime
+        updated_date = unicode(datetime.datetime.now().date())
+        updated_time = unicode(datetime.datetime.now())
+        data[0]["updated_date"] = updated_date
+        data[0]["updated_time"] = updated_time
         data[0]["volume_usd"] = data[0]["24h_volume_usd"]
-        add_db = client.collection(name).document(date_time).set(data[0])
+        add_db = client.collection(name).document(updated_date).set(data[0])
 
-# schedule.every().day.at("10:00").do(sampling_data,'It is 10:00')
+schedule.every().day.at("06:00").do(sampling_data)
+schedule.every().day.at("14:00").do(sampling_data)
+schedule.every().day.at("23:30").do(sampling_data)
+#test
+#schedule.every().day.at("16:20").do(sampling_data)
 def main():
     while True:
-        # schedule.run_pending()
-	sampling_data()
-	time.sleep(10000)
+        schedule.run_pending()
+	#sampling_data()
+	time.sleep(1)
 
 if __name__ == "__main__":
   main()

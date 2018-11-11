3d220c94bfdfc0c1025ff9ab041894a12bc785ae
admin-compute <hodinhtan@gmail.com>
2018-11-11 14:30:11 +0000

    Code::Task5 update json format

--- CoinMarketCap/main.py
+++ CoinMarketCap/main.py
@@ -4,7 +4,7 @@ import os
 import time
 from google.cloud import firestore
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/hodinhtan/volume24bot/cmarket/keyfilecloud.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/hodinhtan/volume24bot/CoinMarketCap/keyfilecloud.json"
 #/home/tanho/keyfile.json
 # "/home/tanho/coinpy/volume24bot/cmarket/keyfilecloud.json
 #/home/tanho/github/py/volume24bot/cmarket
@@ -17,21 +17,23 @@ def urlCoin(name):
 
 import datetime, schedule
 
-def sampling_data() :
-	for name in lt:
+def sampling_data():
+    for name in lt:
         req = urlCoin(name)
         data = req.json()
         date_time = unicode(datetime.datetime.now().date())
         updatedtime = unicode(datetime.datetime.now())
-        data[0].append({"datetime":date_time, "updatedtime": updatedtime})
+        data[0]["datetime"] = date_time
+        data[0]["updatedtime"] = updatedtime
+        data[0]["volume_usd"] = data[0]["24h_volume_usd"]
         add_db = client.collection(name).document(date_time).set(data[0])
 
 # schedule.every().day.at("10:00").do(sampling_data,'It is 10:00')
 def main():
-	while True:
-		# schedule.run_pending()
-		sampling_data()
-		sleep(10000)
+    while True:
+        # schedule.run_pending()
+	sampling_data()
+	time.sleep(10000)
 
 if __name__ == "__main__":
   main()
